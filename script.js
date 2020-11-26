61b21095da963357824b1e8dcd22bffca4e49677
jamesnascimento1994 <james.nascimento94@yahoo.com>
2020-11-26 02:02:33 -0500

    completed functions

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// all my elements
 let h1 = document.querySelector('.header')
 let startButton = document.querySelector('.start-button');
 console.log(startButton);
 startButton.addEventListener('click', startGame);
+let instructionHeader = document.querySelector('#instruction-header');
+let instructions1 = document.querySelector('.instructions1');
+let instructions2 = document.querySelector('.instructions2');
 let net = document.querySelector('#net');
 let scoreOne = document.querySelector('#score1');
 let scoreTwo = document.querySelector('#score2');
+let paddleOne = document.querySelector('#paddle1');
+let paddleTwo = document.querySelector('#paddle2');
 let theBall = document.querySelector('#ball');
 let resetButton = document.querySelector('.reset-button');
 resetButton.addEventListener('click', resetGame);
@@ -22,9 +28,21 @@ let topSpeedOfBall = 10;
 let leftSpeedOfBall = 0;
 let score1 = 0;
 let score2 = 0;
+let winningScore = 7;
 
+// The function that occurs when the start button is clicked
+function startGame(e) {
+	e.preventDefault();
+	startTimer();
+	startButton.style.display = 'none';
+	instructionHeader.style.display = 'none';
+	instructions1.style.display = 'none';
+	instructions2.style.display = 'none';
+}
+
+// a 3 second countdown initiated after the start button is pushed
 function startTimer() {
-	let counter = 3;
+	let counter = 4;
 	setInterval(function() {
 		counter--;
 		if (counter >= 0) {
@@ -44,18 +62,11 @@ function startTimer() {
 	}, 1000);
 }
 
-function startGame(e) {
-	e.preventDefault();
-	startTimer();
-	startButton.style.display = 'none';
-}
-
+// The reset function
 function resetGame(e) {
 	e.preventDefault();
 	document.location.href = "";
 }
-
-// 2 players
 
 function startBall() {
 	document.querySelector('#countdown').style.display = 'none';
@@ -146,6 +157,8 @@ window.setInterval(function show() {
 			topPositionOfBall < positionOfPaddle1 + paddleHeight
 		) {
 			leftSpeedOfBall = -leftSpeedOfBall;
+			let audio = new Audio('audio/punch.wav');
+			audio.play();
 		} else {
 			score2++;
 			startBall();
@@ -158,10 +171,39 @@ window.setInterval(function show() {
 			topPositionOfBall < positionOfPaddle2 + paddleHeight
 		) {
 			leftSpeedOfBall = -leftSpeedOfBall;
+			let audio = new Audio('audio/punch.wav');
+			audio.play();
 		} else {
 			score1++;
 			startBall();
 		}
+	}
+
+	if (winningScore === score1) {
+		
+		document.querySelector('.winning-statement').innerText = "Player 1 Wins!"
+		paddleOne.style.display = 'none';
+		paddleTwo.style.display = 'none';
+		theBall.style.display = 'none';
+		net.style.display = 'none';
+		scoreOne.style.display = 'none';
+		scoreTwo.style.display = 'none';
+		score1 = 0;
+		score2 = 0;
+		let audio = new Audio('audio/applause6.mp3')
+		audio.play();
+	} else if (winningScore === score2) {
+		document.querySelector('.winning-statement').innerText = 'Player 2 Wins!';
+		paddleOne.style.display = 'none';
+		paddleTwo.style.display = 'none';
+		theBall.style.display = 'none';
+		net.style.display = 'none';
+		scoreOne.style.display = 'none';
+		scoreTwo.style.display = 'none';
+		score1 = 0;
+		score2 = 0;
+		let audio = new Audio('audio/applause6.mp3');
+		audio.play();
 	}
 
 	document.getElementById('paddle1').style.top = positionOfPaddle1 + 'px';
